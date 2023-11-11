b22a300ef752bc6c0e5e0f1981b7c60f27e2f07d
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-11 23:48:51 +0800

    Add yup validation

---
+++ src/validation/index.js
@@ -0,0 +1,30 @@
+
+import createCommentValidation from "./createComment";
+import createDeliveryValidation from "./createDelivery";
+import createProductValidation from "./createProduct";
+import createTransactionValidation from "./createTransaction";
+import createUserValidation from "./createUser";
+import editCommentValidation from "./editComment";
+import editDeliveryValidation from "./editDelivery";
+import editProductValidation from "./editProduct";
+import editTransactionValidation from "./editTransaction";
+import editUserValidation from "./editUser";
+import forgotPasswordValidation from "./forgotPassword";
+import loginUserValidation from "./loginUser";
+import resetPasswordValidation from "./resetPassword";
+
+export {
+  createCommentValidation,
+  createDeliveryValidation,
+  createProductValidation,
+  createTransactionValidation,
+  createUserValidation,
+  editCommentValidation,
+  editDeliveryValidation,
+  editProductValidation,
+  editTransactionValidation,
+  editUserValidation,
+  forgotPasswordValidation,
+  loginUserValidation,
+  resetPasswordValidation,
+};
