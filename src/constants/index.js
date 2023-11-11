f1b154da055d7eb1bdbfc0b02c645464a3d45be9
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-11 22:46:08 +0800

    Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+import TAGS from "./tags";
+import ROUTE from "./route";
+import API from "./api";
+import ROLES from "./roles";
+import USER from "./user";
+import STATUS from "./status";
+
+export {
+  TAGS,
+  ROUTE,
+  API,
+  ROLES,
+  USER,
+  STATUS,
+};
