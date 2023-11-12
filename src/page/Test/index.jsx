ca56ab645e8d82a4dedbccbea81c5e23ffca4789
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-12 20:09:55 +0800

    Add Test

---
+++ src/page/Test/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function () {
+
+  return (
+    <>
+      <div className="grid justify-center items-center h-screen">
+        <h1>Login Works!</h1>
+      </div>
+    </>
+  );
+}
