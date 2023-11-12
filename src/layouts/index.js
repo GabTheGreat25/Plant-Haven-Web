bc349ff01592cbff35f77f61a102f58735ad6d64
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-12 20:09:08 +0800

    Import new layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,17 @@
 import RootLayout from "./RootLayout";
 import NotFound from "./NotFound";
 import Welcome from "./Welcome";
-import MainLayout from "./MainLayout";
+import HomeLayout from "./HomeLayout";
+import AdminLayout from "./AdminLayout";
+import EmployeeLayout from "./EmployeeLayout";
+import CustomerLayout from "./CustomerLayout";
 
-export { RootLayout, NotFound, Welcome, MainLayout };
+export {
+    RootLayout,
+    NotFound,
+    Welcome,
+    HomeLayout,
+    AdminLayout,
+    EmployeeLayout,
+    CustomerLayout
+};
