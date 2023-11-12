3bada0c29e98e2dceae85ddb67f136e3b84a8fcf
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-12 22:36:56 +0800

    Add new import

--- src/page/index.js
+++ src/page/index.js
@@ -2,10 +2,12 @@ import LoginUser from "./LoginUser";
 import Test from "./Test";
 import CustomerRegister from "./CustomerRegister";
 import EmployeeRegister from "./EmployeeRegister";
+import UpdateUserInfo from "./User/UpdateUserInfo";
 
 export {
     LoginUser,
     Test,
     CustomerRegister,
-    EmployeeRegister
+    EmployeeRegister,
+    UpdateUserInfo,
 };
