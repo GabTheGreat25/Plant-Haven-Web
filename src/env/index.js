fbc8d665aef307d24e4500c2b88225e942d40c44
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-15 16:02:25 +0800

    Add url if with internet or not

--- src/env/index.js
+++ src/env/index.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:4000/api/v1/";
+export const API_URL = window.navigator.onLine
+  ? "https://plant-haven-api.onrender.com/api/v1/"
+  : "http://localhost:4000/api/v1/";
